817c4ef77993f3bda08a73269af469cabf96cf89
Gary <gortiz1490@gmail.com>
2020-12-28 20:27:36 -0600

    update

--- API/app.js
+++ API/app.js
@@ -64,13 +64,11 @@ app.get("/getdescargas", (req, res) => {
 app.get("/getusuario", (req, res) => {
 
     var user_id = req.query.auxiliar
-    console.log(user_id)
 
     var sql = `SELECT * FROM usuario where id_usuario='${user_id}'`;
-    console.log(sql)
     connection.query(sql, function(err, result) {
         if (err) res.sendStatus(404);
-        res.send(result)
+        res.json(result)
     });
 });
 
